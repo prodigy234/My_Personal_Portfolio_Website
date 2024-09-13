4733c3e72080595718d666e2fbae5fedeacc0ae8
prodigy234 <k.gbenga234@gmail.com>
2024-09-13 03:54:09 +0100

    My Fifth Code Push

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0)
 })
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
